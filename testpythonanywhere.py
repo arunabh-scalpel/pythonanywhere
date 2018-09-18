d6d2fa70284cca63c0af73e4dd55fd8014117a1a
Arunabh <arunabh.majumdar@gmail.com>
2018-09-19 01:09:39 +0530

    ok21

--- testpythonanywhere.py
+++ testpythonanywhere.py
@@ -8,12 +8,16 @@ Created on Wed Sep 19 00:21:12 2018
 
 import re
 import pandas as pd
+
+
+country = input("Enter your country: ")
+capital = None
 sample={}
 sample["India"] = "Delhi"
 sample["Pakistan"] = "Karachi"
 
-country = input("Enter your country: ")
-capital = None
-
-if country in sample:
-    print ("The capital of "+country+" is "+sample[country])
+#if country in sample:
+#    print ("The capital of "+country+" is "+sample[country])
+#
+#print (sample)
+print (country)
